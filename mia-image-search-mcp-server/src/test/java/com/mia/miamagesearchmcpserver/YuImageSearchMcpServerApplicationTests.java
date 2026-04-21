3c4c60647732ead33e2c3899c45083a3eee94462
choose <47557729+MiaChoose@users.noreply.github.com>
2026-04-21 18:54:21 +0800

    Add files via upload
    AI-mate-AI-agent-终

---
+++ mia-image-search-mcp-server/src/test/java/com/mia/miamagesearchmcpserver/YuImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mia.miamagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaImageSearchMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
